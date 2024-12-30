0640ccac1fc12843d05ac71e0d16d928599d4a2d
Sylvestre <silisipro@gmail.com>
2024-12-30 07:07:16 -0500

    maj

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -1,17 +1,19 @@
-// mealsSlice.js
-import { createSlice } from '@reduxjs/toolkit';
 
+import { createSlice } from '@reduxjs/toolkit';
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
-   
+    { name: 'Petit-déjeuner', cost: 50, selected: false },
+    { name: 'Thé de l’après-midi', cost: 25, selected: false },
+    { name: 'Déjeuner', cost: 65, selected: false },
+    { name: 'Dîner', cost: 70, selected: false },
   ],
   reducers: {
     toggleMealSelection: (state, action) => {
+        state[action.payload].selected = !state[action.payload].selected;
     },
   },
 });
 
 export const { toggleMealSelection } = mealsSlice.actions;
-
 export default mealsSlice.reducer;
